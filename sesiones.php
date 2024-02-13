6e4096c26599f601e8301e0c132c12241315bfd8
david <dsobped691a@ieslacampina.es>
2024-02-13 14:35:03 +0100

    sesiones con navegador

--- sesiones.php
+++ sesiones.php
@@ -13,15 +13,15 @@ $fila2 = $resultado2->fetch_assoc();
 <html lang="es">
 
 <head>
-    	<!-- Required meta tags -->
+	<!-- Required meta tags -->
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 	<!-- Bootstrap CSS -->
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/jquery.dataTables.min.css">
-	<link rel="shortcut icon" href="images/icono.png">
 	<link rel="stylesheet" href="estilos.css">
+	<link rel="shortcut icon" href="images/icono.png">
 
 	<!-- Optional JavaScript -->
 	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
@@ -31,15 +31,46 @@ $fila2 = $resultado2->fetch_assoc();
 
 	<title>Gimnasio No+Panza</title>
 
+	<!-- <script>
+		$(document).ready( function () {
+			$('#tabla').DataTable();
+		} );
+	</script> -->
+	<header>
+		
+	<nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
+  <h1 class="navbar-brand">Gimnasio No+Panza</h1>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Inicio <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Acciones
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="registrar.php">Registrar</a>
+          <a class="dropdown-item" href="error.php">Proximamente</a>
+        </div>
+      </li>
+    </ul>
+	<a href="error.php" class="form-inline my-2 my-lg-0 nav-link">Login</a>
+  </div>
+</nav>
+	</header>
+
 	<script>
 		$(document).ready( function () {
 			$('#tabla').DataTable();
 		} );
 	</script>
-
-    <title>Partidas jugadas</title>
-   
 </head>
+
 
 <body>
 	<h1>Sesiones de <?php echo $fila2['Nombre']; ?></h1>
