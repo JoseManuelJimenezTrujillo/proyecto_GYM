23731898d60755e50ff38cc0128bf5acf7d0d496
david <dsobped691a@ieslacampina.es>
2024-02-13 14:33:10 +0100

    correccion error.php

--- error.php
+++ error.php
@@ -1,21 +1,64 @@
 <?php
-require conexion.php
+require 'conexion.php';
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-		<link rel="stylesheet" href="estilos.css">
-        <link rel="shortcut icon" href="images/icono.png">
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-		<title>Gimnasio No+Panza</title>
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/jquery.dataTables.min.css">
+	<link rel="stylesheet" href="estilos.css">
+	<link rel="shortcut icon" href="images/icono.png">
+
+	<!-- Optional JavaScript -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="js/jquery-3.4.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.dataTables.min.js"></script>
+
+	<title>Gimnasio No+Panza</title>
+
+	<!-- <script>
+		$(document).ready( function () {
+			$('#tabla').DataTable();
+		} );
+	</script> -->
+	<header>
+		
+	<nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
+  <h1 class="navbar-brand">Gimnasio No+Panza</h1>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Inicio <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Acciones
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="registrar.php">Registrar</a>
+          <a class="dropdown-item" href="error.php">Proximamente</a>
+        </div>
+      </li>
+    </ul>
+	<a href="error.php" class="form-inline my-2 my-lg-0 nav-link">Login</a>
+  </div>
+</nav>
+	</header>
+
 </head>
 <body>
 <h1>OOPS! Vuelva mas tarde</h1>
     <img src="images/ups.jpg" alt="ups">
-    <p><a href="../">Volver</a></p>
 </body>
 </html>
