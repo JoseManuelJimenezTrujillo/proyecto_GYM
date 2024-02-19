a0096655d8db806d1ad261415df195f5dd53a32e
ZedCrimson <sgalanbrenes@gmail.com>
2024-02-19 14:13:19 +0100

    header

--- modificar.php
+++ modificar.php
@@ -8,6 +8,33 @@
 ?>
 <!doctype html>
 <html lang="es">
+<header>
+		
+		<nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
+	  <h1 class="navbar-brand">Gimnasio No+Panza</h1>
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	  </button>
+	
+	  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+		<ul class="navbar-nav mr-auto">
+		  <li class="nav-item active">
+			<a class="nav-link" href="index.php">Inicio <span class="sr-only">(current)</span></a>
+		  </li>
+		  <li class="nav-item dropdown">
+			<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			  Acciones
+			</a>
+			<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+			  <a class="dropdown-item" href="registrar.php">Registrar</a>
+			  <a class="dropdown-item" href="#">Proximamente</a>
+			</div>
+		  </li>
+		</ul>
+		<a href="login.php" class="form-inline my-2 my-lg-0 nav-link">Login</a>
+	  </div>
+	</nav>
+		</header>
 	<head>
 		<!-- Required meta tags -->
 		<meta charset="utf-8">
