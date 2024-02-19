c5838bcb6432f59f1539229a55a41a7463e04570
david <dsobped691a@ieslacampina.es>
2024-02-19 14:47:14 +0100

    ultimo commin, NO MAS!!!!

--- index.php
+++ index.php
@@ -9,36 +9,13 @@
 	<link rel="stylesheet" href="css/jquery.dataTables.min.css">
 	<link rel="stylesheet" href="login.css">
 	<link rel="shortcut icon" href="images/icono.png">
-	<header>
-		<nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
-			<h1 class="navbar-brand">Gimnasio No+Panza</h1>
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
-
-			<div class="collapse navbar-collapse" id="navbarSupportedContent">
-				<ul class="navbar-nav mr-auto">
-					<li class="nav-item active">
-						<a class="nav-link" href="index.php">Inicio <span class="sr-only">(current)</span></a>
-					</li>
-					<li class="nav-item dropdown">
-						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-							Acciones
-						</a>
-						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-							<a class="dropdown-item" href="registrar.php">Registrar</a>
-							<a class="dropdown-item" href="proximamente.php">Proximamente</a>
-						</div>
-					</li>
-				</ul>
-			
-			</div>
-		</nav>
-	</header>
+	
 	<title>Gimnasio No+Panza Login</title>
 </head>
 
 <body>
+
+    <div class="login">
 	<div class="container">
 		<div class="logo">Gimnasio No+Panza</div>
 		<form class="form" method="POST" action="login.php">
@@ -47,7 +24,7 @@
 			<button type="submit" class="button">Iniciar sesión</button>
 		</form>
 		
-
+        </div>
 	</div>
 </body>
 
